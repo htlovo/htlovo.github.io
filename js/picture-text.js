203945d064aedae23690b462be13deb11d813d44
htlovo <1056442858@qq.com>
2017-03-14 22:44:31 +0800

    js example ,documents

---
+++ js/picture-text.js
@@ -0,0 +1,8 @@
+document.write{hloie}
+var sec=document.getElementsByTagName('section');
+for (var i = 0; i < sec.length; i++) {
+	sec[i].onclick=function sec(i) {
+		// body...
+		sec[i].img.display=block;
+	}
+}
