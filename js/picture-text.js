deed14d9c36cb948f64026740595cbb1bdb282c1
htlovo <1056442858@qq.com>
2017-03-15 12:29:52 +0800

    picture-text

--- js/picture-text.js
+++ js/picture-text.js
@@ -1,8 +1,17 @@
-document.write{hloie}
-var sec=document.getElementsByTagName('section');
-for (var i = 0; i < sec.length; i++) {
-	sec[i].onclick=function sec(i) {
-		// body...
-		sec[i].img.display=block;
+function a() {
+	
+	var arc=document.getElementsByClassName('picture-text')[0];
+	var li=arc.getElementsByTagName('li');
+	for (var i = 0; i < li.length; i++) {
+		li[i].onclick=function(){b(this);}
 	}
 }
+function b(c) {
+	var arc=document.getElementsByClassName('picture-text')[0];
+	var li=arc.getElementsByTagName('li');
+	for (var i = 0; i < li.length; i++) {
+		li[i].getElementsByTagName("img")[0].setAttribute("style","display:none;");
+	}
+	c.getElementsByTagName("img")[0].setAttribute("style","display:block;float:right;");
+}
+window.onload=a;
