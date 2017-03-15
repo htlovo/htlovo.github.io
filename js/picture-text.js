9872927e169dcc44546692cc4ea81e8c88a79d13
htlovo <1056442858@qq.com>
2017-03-15 20:41:41 +0800

    plan,blue

--- js/picture-text.js
+++ js/picture-text.js
@@ -1,5 +1,4 @@
 function a() {
-	
 	var arc=document.getElementsByClassName('picture-text')[0];
 	var li=arc.getElementsByTagName('li');
 	for (var i = 0; i < li.length; i++) {
@@ -10,8 +9,11 @@ function b(c) {
 	var arc=document.getElementsByClassName('picture-text')[0];
 	var li=arc.getElementsByTagName('li');
 	for (var i = 0; i < li.length; i++) {
-		li[i].getElementsByTagName("img")[0].setAttribute("style","display:none;");
+		var d=li[i].getElementsByTagName("img")[0];
+		d.setAttribute("style","display:none;");
 	}
-	c.getElementsByTagName("img")[0].setAttribute("style","display:block;float:right;");
+	d=c.getElementsByTagName("img")[0];
+	d.style.display="block";
+	d.style.float="right"
 }
 window.onload=a;
