41b55fa1e2a463aae9604a492b29049b5db40416
htlovo <1056442858@qq.com>
2017-03-18 13:24:14 +0800

    article

--- js/picture-text.js
+++ js/picture-text.js
@@ -2,6 +2,8 @@ function a() {
 	var arc=document.getElementsByClassName('picture-text')[0];
 	var li=arc.getElementsByTagName('li');
 	for (var i = 0; i < li.length; i++) {
+		var d=li[i].getElementsByTagName("img")[0];
+		d.style.display="none"
 		li[i].onclick=function(){b(this);}
 	}
 }
@@ -10,10 +12,9 @@ function b(c) {
 	var li=arc.getElementsByTagName('li');
 	for (var i = 0; i < li.length; i++) {
 		var d=li[i].getElementsByTagName("img")[0];
-		d.setAttribute("style","display:none;");
+		d.style.display="none";
 	}
 	d=c.getElementsByTagName("img")[0];
 	d.style.display="inline-block";
-	d.style.cssFloat="right";
 }
 window.onload=a;
