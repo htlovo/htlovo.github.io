627a0c4e0867972fa534e1acc3706ff022b78b2c
htlovo <1056442858@qq.com>
2017-03-23 13:58:59 +0800

    plan picture

--- js/picture-text.js
+++ js/picture-text.js
@@ -2,19 +2,37 @@ function a() {
 	var arc=document.getElementsByClassName('picture-text')[0];
 	var li=arc.getElementsByTagName('li');
 	for (var i = 0; i < li.length; i++) {
-		var d=li[i].getElementsByTagName("img")[0];
-		d.style.display="none"
+		var d=li[i].getElementsByTagName("img");
+			for (var j = 0; j < d.length; j++) {
+				d[j].style.display="none";
+			}
 		li[i].onclick=function(){b(this);}
 	}
 }
-function b(c) {
+/*function b(c) {
 	var arc=document.getElementsByClassName('picture-text')[0];
 	var li=arc.getElementsByTagName('li');
 	for (var i = 0; i < li.length; i++) {
-		var d=li[i].getElementsByTagName("img")[0];
-		d.style.display="none";
+		var d=li[i].getElementsByTagName("img");
+		for (var j = 0; j < d.length; j++) {
+				d[j].style.display="none";
+			}
 	}
-	d=c.getElementsByTagName("img")[0];
-	d.style.display="inline-block";
+	d=c.getElementsByTagName("img");
+	for (var j = 0; j < d.length; j++) {
+		d[j].style.display="inline-block";
+	}
+}*/
+function b(c) {
+	var d=c.getElementsByTagName("img");
+	if (d[0].style.display=="none") {
+		for (var j = 0; j < d.length; j++) {
+			d[j].style.display="inline-block";
+		}
+	} else {
+		for (j = 0; j < d.length; j++) {
+			d[j].style.display="none";
+		}
+	}
 }
 window.onload=a;
